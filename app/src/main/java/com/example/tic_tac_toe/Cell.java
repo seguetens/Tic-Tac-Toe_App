8197c3368e88c4557ee5cbb803e3110154cd8cb2
seguetens <seguetens@loyola.edu>
2020-10-30 19:22:36 -0400

    layout v7

---
+++ app/src/main/java/com/example/tic_tac_toe/Cell.java
@@ -0,0 +1,9 @@
+package com.example.tic_tac_toe;
+
+import android.widget.Button;
+
+public class Cell {
+
+    public Button cell;
+
+}
--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.tic_tac_toe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_startup);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Choice made");
     }
 }
