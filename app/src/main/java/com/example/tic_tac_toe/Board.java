cc98fb4e30417522429843bf649be5aff437100c
seguetens <seguetens@loyola.edu>
2020-10-30 22:56:24 -0400

    v9

--- app/src/main/java/com/example/tic_tac_toe/Board.java
+++ app/src/main/java/com/example/tic_tac_toe/Board.java
@@ -1,3 +1,7 @@
+/**
+ * @author Scott Guetens
+ * @version 1.0
+ */
 package com.example.tic_tac_toe;
 
 public class Board {
--- app/src/main/java/com/example/tic_tac_toe/Game.java
+++ app/src/main/java/com/example/tic_tac_toe/Game.java
@@ -1,18 +1,21 @@
+/**
+ * @author Scott Guetens
+ * @version 1.0
+ */
 package com.example.tic_tac_toe;
 
 public class Game{
 
-    protected static String player_1;
-    protected static String player_2;
+    protected static String player_1 = "O";
+    protected static String player_2 = "X";
 
     protected static void startGame(int i) {
+
         if (i == 0) {
             player_1 = "X";
             player_2 = "O";
         }
-        else {
-            player_1 = "O";
-            player_2 = "X";
-        }
+
+
     }
 }
